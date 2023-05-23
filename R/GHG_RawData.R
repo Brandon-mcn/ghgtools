14e1a7d09734cc649e3f9f3e01b164247ceb1562
Brandon-mcn <brandon_mcn@outlook.com>
2023-05-23 11:06:56 -0700

    May 23 update

--- R/GHG_RawData.R
+++ R/GHG_RawData.R
@@ -11,22 +11,22 @@ GHG_RawData <- function(GWP){
   
   #load in Asset Portfolio
   
-  AssetPortfolio <- read_excel("ghgtools_AssetPortfolio_V1.xlsx", sheet = "AssetPortfolio")
+  AssetPortfolio <- read_excel("ghgtools_AssetPortfolio.xlsx", sheet = "AssetPortfolio")
   
   #load in emission factor library and filter out extra columns
   
-  EFL <-  read_excel("ghgtools_EFL_V1.xlsx", sheet = "EFL") %>% 
+  EFL <-  read_excel("ghgtools_EFL.xlsx", sheet = "EFL") %>% 
     select(-c("Vendor", "ProductID", "EFStartDate", "EFRetireDate")) %>% 
     select(-c("CO2_SourceUnit.per.UOM", "CH4_SourceUnit.per.UOM",	"N2O_SourceUnit.per.UOM", "AUXCO2e_SourceUnit.per.UOM",	"GHGSourceUnit", "Validated?")) %>% 
     select(-starts_with("SubCat"))
   
   #load in scope type mapping
   
-  ScopeType <-  read_excel("ghgtools_EFL_V1.xlsx", sheet = "ScopeType")
+  ScopeType <-  read_excel("OpenEFL.xlsx", sheet = "ScopeType")
   
   #load in eGRID lookup table for electricity subregions
   
-  eGRIDlookup <- read_excel("ghgtools_EFL_V1.xlsx", sheet = "eGRIDlookup") 
+  eGRIDlookup <- read_excel("OpenEFL.xlsx", sheet = "eGRIDlookup") 
   
   #load in GWPs and filter to selected assessment report
   
@@ -41,12 +41,12 @@ GHG_RawData <- function(GWP){
   } else {
     NA
   }
-  GWPs <- read_excel("ghgtools_EFL_V1.xlsx", sheet = "GWPs")
+  GWPs <- read_excel("OpenEFL.xlsx", sheet = "GWPs")
   GWPs <- GWPs[c(1,x)]
   
   #load Activity Data and join to Asset Portfolio
   
-  GHGrawdata <- read_excel("ghgtools_ActivityData_V1.xlsx", sheet = "ActivityData") %>%
+  GHGrawdata <- read_excel("ghgtools_ActivityData.xlsx", sheet = "ActivityData") %>%
     mutate(Year = year(Date)) %>%
     relocate(Year, .after = Date) %>%
     left_join(AssetPortfolio, by = "AssetID") %>%
