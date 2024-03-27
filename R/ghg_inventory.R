d4bca9757c89e9b8970afd54d7d70f7cbb2489b9
Brandon-mcn <brandon_mcn@outlook.com>
2024-03-27 09:47:34 -0700

    3/27 update

--- R/ghg_inventory.R
+++ R/ghg_inventory.R
@@ -21,9 +21,9 @@ EFL_CO2e[EFL_CO2e == ""] <- NA
 setcolorder(EFL_CO2e, c("ef_source",
                         "ef_publishdate",
                         "ef_activeyear",
+                        "emission_category",
                         "service_type",
                         "unit",
-                        "emission_category",
                         "service_subcategory1",
                         "service_subcategory2",
                         "emission_scope",
@@ -41,29 +41,63 @@ setcolorder(EFL_CO2e, c("ef_source",
 EFL_CO2e <- EFL_CO2e[, .(kgco2e_perunit = sum(kgco2e_perunit)), by = .(ef_source, ef_publishdate, ef_activeyear, service_type, unit, emission_category, service_subcategory1, service_subcategory2, emission_scope, country, subregion, ar)]
 setnames(EFL_CO2e, "ef_activeyear", "year")
 
-return(EFL_CO2e)
+fwrite(EFL_CO2e, "EFL.csv")
+
+}
+
+#' create_templates
+#'
+#' This function creates the activity data and asset portfolio templates as CSV files in your project directory.
+#'
+#' @return The activity data and asset portfolio templates
+#' @import data.table
+#' @export
+create_templates <- function(){
+
+  fwrite(ActivityData, "ActivityData.csv")
+  fwrite(AssetPortfolio, "AssetPortfolio.csv")
+  return(c("Success! Check your directory for .csv templates",
+         "Add your data to these templates, making sure not to change the column headers",
+         "With your data in the templates, run `check_load_data` to load your data into r"))
+}
+
+#' check_load_data
+#'
+#' This function checks your input data and loads it into the global environment
+#'
+#' @return The required dataframes for ghgtools
+#' @import data.table
+#' @export
+check_load_data <- function(){
+
+  Activity_Data <- fread("ActivityData.csv")
+  Asset_Portfolio <- fread("AssetPortfolio.csv")
+  EF_Library <- fread("EFL.csv")
+
+  assign("Activity_Data", Activity_Data, envir = .GlobalEnv)
+  assign("Asset_Portfolio", Asset_Portfolio, envir = .GlobalEnv)
+  assign("EF_Library", EF_Library, envir = .GlobalEnv)
+
+  return(c("Success! Check your global environment for 'Activity_Data', 'Asset_Portfolio', and 'EF_Library'",
+           "You are now ready to run 'ghg_inventory' and calculate some GHG emissions!"))
 
 }
 
 #' ghg_inventory
 #'
-#' Calculates a GHG inventory using data in AssetPortfolio and ActivityData. An emission factor library is generated based on your selection of global warming potentials (GWPs). If you have no preference for GWPs, we reccomend following UNFCCC guidelines, which require the use of GWP values from the IPCC's Fifth Assessment Report (AR5).
+#' Calculates a GHG inventory using input data from EF_Library, Asset_Portfolio, and Activity_Data variables in the global environment.
 #'
-#' @seealso [Gudiance from EPA about GWPs](https://www.epa.gov/ghgemissions/understanding-global-warming-potentials)
-#' @param GWP Select your GWPs. Enter with quotation marks in the function. Choices are "SAR", "AR4", "AR5", or "AR6"
 #' @return The GHG raw data
 #' @import data.table
 #' @export
-ghg_inventory <- function(GWP){
+ghg_inventory <- function(){
 
-  EFL_CO2e <- create_efl(GWP)
-
-  DT1 <- ActivityData
-  DT1[DT1 == ""] <- NA
-  DT2 <- data.table(merge.data.table(DT1, AssetPortfolio, sort = FALSE, all.x = TRUE))
+  DT1 <- Activity_Data
+  DT2 <- data.table(merge.data.table(DT1, Asset_Portfolio, sort = FALSE, all.x = TRUE))
   DT3 <- data.table(merge.data.table(DT2, Ecat_lookup, by = c("asset_type", "service_type"), sort = FALSE))
   DT4 <- data.table(merge.data.table(DT3, eGRIDlookup, sort = FALSE, all.x = TRUE))
-  GHGrawdata <- data.table(merge.data.table(DT4, EFL_CO2e, by = c("year",
+  DT4[is.na(DT4)] <- ""
+  GHGrawdata <- data.table(merge.data.table(DT4, EF_Library, by = c("year",
                                                                   "service_type",
                                                                   "emission_category",
                                                                   "service_subcategory1",
@@ -107,6 +141,8 @@ ghg_inventory <- function(GWP){
                             "ef_publishdate"))
   GHGrawdata[is.na(GHGrawdata)] <- ""
 
-  return(GHGrawdata)
+  fwrite(GHGrawdata, "GHGrawdata.csv")
+  assign("GHG_rawdata", GHGrawdata, envir = .GlobalEnv)
+  return(c("Success! Check your directory for 'GHGrawdata.csv'"))
 
 }
