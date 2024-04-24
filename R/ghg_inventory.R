b29384b308407355931984c79c835b8e95a64c59
maxwell-miller-geo <mmm683@nau.edu>
2024-04-24 09:57:10 -0700

    Comments/ Suggestions on GHGTools
    function "create_templates" could specify a folder location to save the templates. If there is no specified location, it can default to a temporary directory. There are some comments and suggestions within the function that might work.
    Good documentation and examples on inputs for the function ghg_inventory().
    
    Within the knitted "ghgtools.Rmd" I think you could show you called the "Activity Data" and the "Asset Portfolio" data sets because it isn't showing the code block right now.
    
    I added some comments to the ghg_inventory and create_templates functions for more details.
    
    Overall, everything works really well. The documentation is good.
    
    Some things to consider or add,
    1) Creating a helper function to check that input columns exist for Activity Data and Asset Portfolio.
    2) Creating a helper function that can use partial matches on strings for items within a table, if things are close enough to match - might be a little time consuming, but it could make it a little more error resistant.
    See "regular expressions in R"
    https://cran.r-project.org/web/packages/stringr/vignettes/regular-expressions.html
    3) Saving the outputs to a specified location; that way, people can work on the datasheets in Excel/wherever and know where they are located.
    
    I hope this helps!

--- R/ghg_inventory.R
+++ R/ghg_inventory.R
@@ -5,12 +5,24 @@
 #' @return The activity data, asset portfolio templates, and the full emission factor library
 #' @import data.table
 #' @export
-create_templates <- function(){
+create_templates <- function(output_location = NULL){
+  # I might specify where you want to save your files/template to
+  # or have an automatic place to save to. If this is meant to be performed locally.
+  # E.G. - potential example totally up to you, just maybe something to think about
+  if(is.null(output_location)){
+    green_house_gas <- "Green House Gas Accounting"
+    out_folder <- file.path(tempdir(), green_house_gas)
+    # Check if directory exists
+    if(!file.exists(out_folder)){
+      dir.create(out_folder) # saves to a temporary directory, can be input manually
+    }
+  }
 
-  fwrite(ActivityData, "ActivityData.csv")
-  fwrite(AssetPortfolio, "AssetPortfolio.csv")
-  fwrite(EFLibrary, "EFLibrary.csv")
-  return("Success! Check your directory for ActivityData.csv, AssetPortfolio.csv, and EFLibrary.csv")
+  data.table::fwrite(ActivityData, file.path(out_folder, "ActivityData.csv"))
+  data.table::fwrite(AssetPortfolio, file.path(out_folder, "AssetPortfolio.csv"))
+  data.table::fwrite(EFLibrary, file.path(out_folder, "EFLibrary.csv"))
+  print("Success! Check your directory for ActivityData.csv, AssetPortfolio.csv, and EFLibrary.csv")
+  return(out_folder)
 }
 
 #' ghg_inventory
@@ -32,23 +44,27 @@ create_templates <- function(){
 #' @import data.table
 #'
 #' @export
-ghg_inventory <- function(AD, AP, GWP, EFL = EFLibrary){
+ghg_inventory <- function(AD, AP, GWP = "AR5", EFL = EFLibrary){
 
   # Create the table of global warming potentials
   gwp_key <- c("ghg",GWP)
-  GWPs <- GWPs[, gwp_key, with = FALSE]
+  GWPs <- GWPs[, gwp_key, with = FALSE] # I think it is best practice to duplicate this dataset
+  # If you rename it and are trying to troubleshoot it within a session, this will
+  # cause errors because you have subsetted the dataset. It did for me at least.
   colnames(GWPs)[2] <- "gwp"
   co2gwp <- GWPs[GWPs$ghg == "CO2", gwp]
   ch4gwp <- GWPs[GWPs$ghg == "CH4", gwp]
   n2ogwp <- GWPs[GWPs$ghg == "N2O", gwp]
 
   # Consolidate the emission factor library into CO2e values
-  EFL1 <- merge.data.table(EFL, GWPs, sort = FALSE, all.x = TRUE)
+  EFL1 <- data.table::merge.data.table(EFL, GWPs, sort = FALSE, all.x = TRUE)
   EFL1[, gwps_ar := GWP]
-  EFL1[, kgco2e_perunit := ghg_perunit*gwp]
+  EFL1[, kgco2e_perunit := ghg_perunit*gwp] # getting error here
+  # Error in ghg_perunit * gwp : non-numeric argument to binary operator
+  # I think it is because gwp is a string
   EFL1[, ef_publishdate := format(ef_publishdate, "%m/%d/%Y")]
   EFL1[, ghg := ifelse(ghg %in% c("CO2", "CH4", "N2O"), ghg, "other_ghgs")]
-  EFL_CO2e <- dcast(EFL1, ef_source +
+  EFL_CO2e <- data.table::dcast(EFL1, ef_source +
                       ef_publishdate +
                       ef_activeyear +
                       service_type +
@@ -70,23 +86,36 @@ ghg_inventory <- function(AD, AP, GWP, EFL = EFLibrary){
   EFL_CO2e[, CO2 := CO2 / co2gwp]
   EFL_CO2e[, CH4 := CH4 / ch4gwp]
   EFL_CO2e[, N2O := N2O / n2ogwp]
-  setnames(EFL_CO2e, "CO2", "co2_kgperunit")
-  setnames(EFL_CO2e, "CH4", "ch4_kgperunit")
-  setnames(EFL_CO2e, "N2O", "n2o_kgperunit")
-  setnames(EFL_CO2e, "other_ghgs", "otherghgs_kgco2eperunit")
-  setnames(EFL_CO2e, "ef_activeyear", "year")
+  data.table::setnames(EFL_CO2e, "CO2", "co2_kgperunit")
+  data.table::setnames(EFL_CO2e, "CH4", "ch4_kgperunit")
+  data.table::setnames(EFL_CO2e, "N2O", "n2o_kgperunit")
+  data.table::setnames(EFL_CO2e, "other_ghgs", "otherghgs_kgco2eperunit")
+  data.table::setnames(EFL_CO2e, "ef_activeyear", "year")
   EFL_CO2e[is.na(EFL_CO2e)] <- ""
 
+  # ** This seems like it could be a separate function - Checking the
+  # Activity Data and the Assets
   # Merge activity data with assset portfolio
-  AD1 <- data.table(merge.data.table(AD, AP, sort = FALSE, all.x = TRUE))
+  AD1 <- data.table::data.table(data.table::merge.data.table(AD, AP, sort = FALSE, all.x = TRUE))
   AD1[, zip := as.numeric(zip)]
 
   # Add column to specify the emission category
-  AD1 <- data.table(merge.data.table(AD1, Ecat_lookup, by = c("asset_type", "service_type"), sort = FALSE))
+  AD1 <- data.table::data.table(data.table::merge.data.table(AD1,
+                                                             Ecat_lookup,
+                                                             by = c("asset_type", "service_type"),
+                                                             sort = FALSE))
 
   # Add column to specify the eGRID subregion for electricity
-  AD1 <- data.table(merge.data.table(AD1, eGRIDlookup, sort = FALSE, all.x = TRUE))
+  AD1 <- data.table::data.table(data.table::merge.data.table(AD1,
+                                                             eGRIDlookup,
+                                                             sort = FALSE,
+                                                             all.x = TRUE))
   AD1[is.na(AD1)] <- ""
+
+  # ** This chunk to above could be separated into a separate function
+  # that is called and returns the "AD1"  table to be merged with EFL_C02e
+  # It might be more work at first, but you could have some checks within that
+  # function that could determine if the input data is clean and up to snuff
 
   # Merge the emission factor library with activity data
   GHGrawdata <- data.table(merge.data.table(AD1, EFL_CO2e, by = c("year",
