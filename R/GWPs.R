7e95f1cc5795e4cfeeb3576ee6ad562f72a1a1b1
Brandon McNamara <brandon_mcn@outlook.com>
2024-04-30 14:03:35 -0700

    vignette update 2

--- R/GWPs.R
+++ R/GWPs.R
@@ -1,15 +1,16 @@
 #' Global Warming Potentials
 #'
-#' Details...
-#'
+#' A table of all global warming potentials for greenhouse gasses included in the EFLibrary
 #'
 #' @format ## `GWPs`
-#' A data frame with 7,240 rows and 60 columns:
+#' A data frame with 132 rows and 6 columns:
 #' \describe{
-#'   \item{country}{Country name}
-#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
-#'   \item{year}{Year}
+#'   \item{ghg}{Greenhouse gas}
+#'   \item{SAR}{IPCC second assessment report GWPs}
+#'   \item{TAR}{IPCC third assessment report GWPs}
+#'   \item{AR4}{IPCC fourth assessment report GWPs}
+#'   \item{AR5}{IPCC fifth assessment report GWPs}
+#'   \item{source}{The source of GWP data}
 #'   ...
 #' }
-#' @source <ghgtools.io>
 "GWPs"
